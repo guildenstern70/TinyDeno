a7dd46d95369200749cd151c1ec5f56853476e7f
guildenstern70 <alessiosaltarin@gmail.com>
2022-07-20 11:30:20 +0200

    First commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+const router = new Router();
+router.get("/", (ctx) => {
+    ctx.response.body = "Hello world!";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+    "listen",
+    (e) => console.log("Listening on http://localhost:8080"),
+);
+await app.listen({ port: 8080 });
+
